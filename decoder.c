b9681344f0f248627503a4fe5176294172016ba9
josh19280 <joshyeh0808@gmail.com>
2025-12-28 19:45:03 +0800

    Update encoder, decoder workflows

--- decoder.c
+++ decoder.c
@@ -260,7 +260,386 @@ int main(int argc, char *argv[])
             for(int i=0;i<height;i++) free(image[i]);
             free(image);
         }
-        else return 1;
+    }
+/* ================= method 2 ================= */
+    else if (method == 2) {
+        int is_ascii = (strcmp(argv[3], "ascii") == 0);
+
+        if (is_ascii) {
+            // Method 2(a): ASCII format
+            FILE *fin = fopen(argv[4], "r");
+            if (!fin) return 1;
+
+            int width, height;
+            fscanf(fin, "%d %d", &width, &height);
+
+            int num_blocks_h = (height + 7)/8;
+            int num_blocks_w = (width  + 7)/8;
+
+            // Zigzag table
+            int zigzag[64] = {
+                0,  1,  5,  6, 14, 15, 27, 28,
+                2,  4,  7, 13, 16, 26, 29, 42,
+                3,  8, 12, 17, 25, 30, 41, 43,
+                9, 11, 18, 24, 31, 40, 44, 53,
+               10, 19, 23, 32, 39, 45, 52, 54,
+               20, 22, 33, 38, 46, 51, 55, 60,
+               21, 34, 37, 47, 50, 56, 59, 61,
+               35, 36, 48, 49, 57, 58, 62, 63
+            };
+
+            short ***qF_Y  = malloc(num_blocks_h * sizeof(short**));
+            short ***qF_Cb = malloc(num_blocks_h * sizeof(short**));
+            short ***qF_Cr = malloc(num_blocks_h * sizeof(short**));
+
+            for (int m=0; m<num_blocks_h; m++) {
+                qF_Y[m]  = malloc(num_blocks_w * sizeof(short*));
+                qF_Cb[m] = malloc(num_blocks_w * sizeof(short*));
+                qF_Cr[m] = malloc(num_blocks_w * sizeof(short*));
+                for (int n=0; n<num_blocks_w; n++) {
+                    qF_Y[m][n]  = calloc(64, sizeof(short));
+                    qF_Cb[m][n] = calloc(64, sizeof(short));
+                    qF_Cr[m][n] = calloc(64, sizeof(short));
+                }
+            }
+
+            // Read RLE data
+            for (int by=0; by<num_blocks_h; by++) {
+                for (int bx=0; bx<num_blocks_w; bx++) {
+                    int m, n;
+                    char ch;
+
+                    // Y channel
+                    fscanf(fin, " (%d,%d, %c)", &m, &n, &ch);
+                    int pos = 0;
+                    while (1) {
+                        int skip, value;
+                        fscanf(fin, "%d %d", &skip, &value);
+                        if (skip == 0 && value == 0) break;
+                        pos += skip;
+                        if (pos < 64) qF_Y[by][bx][pos++] = value;
+                    }
+
+                    // Cb channel
+                    fscanf(fin, " (%d,%d, %c%c)", &m, &n, &ch, &ch);
+                    pos = 0;
+                    while (1) {
+                        int skip, value;
+                        fscanf(fin, "%d %d", &skip, &value);
+                        if (skip == 0 && value == 0) break;
+                        pos += skip;
+                        if (pos < 64) qF_Cb[by][bx][pos++] = value;
+                    }
+
+                    // Cr channel
+                    fscanf(fin, " (%d,%d, %c%c)", &m, &n, &ch, &ch);
+                    pos = 0;
+                    while (1) {
+                        int skip, value;
+                        fscanf(fin, "%d %d", &skip, &value);
+                        if (skip == 0 && value == 0) break;
+                        pos += skip;
+                        if (pos < 64) qF_Cr[by][bx][pos++] = value;
+                    }
+                }
+            }
+            fclose(fin);
+
+            // Un-DPCM
+            for (int by=1; by<num_blocks_h; by++) {
+                qF_Y[by][0][0]  += qF_Y[by-1][0][0];
+                qF_Cb[by][0][0] += qF_Cb[by-1][0][0];
+                qF_Cr[by][0][0] += qF_Cr[by-1][0][0];
+            }
+            for (int by=0; by<num_blocks_h; by++) {
+                for (int bx=1; bx<num_blocks_w; bx++) {
+                    qF_Y[by][bx][0]  += qF_Y[by][bx-1][0];
+                    qF_Cb[by][bx][0] += qF_Cb[by][bx-1][0];
+                    qF_Cr[by][bx][0] += qF_Cr[by][bx-1][0];
+                }
+            }
+
+            // Load quantization tables
+            unsigned char QtY[8][8] = {
+                {16,11,10,16,24,40,51,61},
+                {12,12,14,19,26,58,60,55},
+                {14,13,16,24,40,57,69,56},
+                {14,17,22,29,51,87,80,62},
+                {18,22,37,56,68,109,103,77},
+                {24,35,55,64,81,104,113,92},
+                {49,64,78,87,103,121,120,101},
+                {72,92,95,98,112,100,103,99}
+            };
+
+            unsigned char QtC[8][8] = {
+                {17,18,24,47,99,99,99,99},
+                {18,21,26,66,99,99,99,99},
+                {24,26,56,99,99,99,99,99},
+                {47,66,99,99,99,99,99,99},
+                {99,99,99,99,99,99,99,99},
+                {99,99,99,99,99,99,99,99},
+                {99,99,99,99,99,99,99,99},
+                {99,99,99,99,99,99,99,99}
+            };
+
+            unsigned char **image = malloc(height * sizeof(unsigned char*));
+            for (int i=0; i<height; i++) image[i] = malloc(width * 3);
+
+                    // Decode each block
+            for (int by=0; by<num_blocks_h; by++) {
+                for (int bx=0; bx<num_blocks_w; bx++) {
+                    double FY[8][8], FCb[8][8], FCr[8][8];
+                    double bY[8][8], bCb[8][8], bCr[8][8];
+
+                    // 先把 zigzag 順序的資料轉回 row-major 順序
+                    short tempY[64], tempCb[64], tempCr[64];
+                    
+                    // Un-zigzag: qF_Y[by][bx][k] 是 zigzag 順序的第 k 個值
+                    // 應該放回 zigzag[k] 這個位置（row-major index）
+                    for (int k=0; k<64; k++) {
+                        int pos = zigzag[k];  // 這是在 64 個元素中的位置
+                        tempY[pos]  = qF_Y[by][bx][k];
+                        tempCb[pos] = qF_Cb[by][bx][k];
+                        tempCr[pos] = qF_Cr[by][bx][k];
+                    }
+
+                    // 現在把 row-major 的資料轉成 2D 並反量化
+                    for (int i=0; i<8; i++) {
+                        for (int j=0; j<8; j++) {
+                            FY[i][j]  = (double)tempY[i*8+j] * QtY[i][j];
+                            FCb[i][j] = (double)tempCb[i*8+j] * QtC[i][j];
+                            FCr[i][j] = (double)tempCr[i*8+j] * QtC[i][j];
+                        }
+                    }
+
+                    IDCT8x8(FY, bY);
+                    IDCT8x8(FCb, bCb);
+                    IDCT8x8(FCr, bCr);
+
+                    for (int i=0; i<8; i++) {
+                        for (int j=0; j<8; j++) {
+                            int py = by*8+i, px = bx*8+j;
+                            if (py<height && px<width) {
+                                unsigned char R, G, B;
+                                YCbCr2RGB(bY[i][j], bCb[i][j], bCr[i][j], &R, &G, &B);
+                                image[py][px*3+0] = B;
+                                image[py][px*3+1] = G;
+                                image[py][px*3+2] = R;
+                            }
+                        }
+                    }
+                }
+            }
+
+            write_bmp(argv[2], image, width, height);
+
+            for (int i=0; i<height; i++) free(image[i]);
+            free(image);
+
+            for (int m=0; m<num_blocks_h; m++) {
+                for (int n=0; n<num_blocks_w; n++) {
+                    free(qF_Y[m][n]);
+                    free(qF_Cb[m][n]);
+                    free(qF_Cr[m][n]);
+                }
+                free(qF_Y[m]);
+                free(qF_Cb[m]);
+                free(qF_Cr[m]);
+            }
+            free(qF_Y);
+            free(qF_Cb);
+            free(qF_Cr);
+
+        } else {
+            // Method 2(b): Binary format
+            FILE *fin = fopen(argv[4], "rb");
+            if (!fin) return 1;
+
+            int width, height;
+            fread(&width, sizeof(int), 1, fin);
+            fread(&height, sizeof(int), 1, fin);
+
+            int num_blocks_h = (height + 7)/8;
+            int num_blocks_w = (width  + 7)/8;
+
+            int zigzag[64] = {
+                0,  1,  5,  6, 14, 15, 27, 28,
+                2,  4,  7, 13, 16, 26, 29, 42,
+                3,  8, 12, 17, 25, 30, 41, 43,
+                9, 11, 18, 24, 31, 40, 44, 53,
+               10, 19, 23, 32, 39, 45, 52, 54,
+               20, 22, 33, 38, 46, 51, 55, 60,
+               21, 34, 37, 47, 50, 56, 59, 61,
+               35, 36, 48, 49, 57, 58, 62, 63
+            };
+
+            short ***qF_Y  = malloc(num_blocks_h * sizeof(short**));
+            short ***qF_Cb = malloc(num_blocks_h * sizeof(short**));
+            short ***qF_Cr = malloc(num_blocks_h * sizeof(short**));
+
+            for (int m=0; m<num_blocks_h; m++) {
+                qF_Y[m]  = malloc(num_blocks_w * sizeof(short*));
+                qF_Cb[m] = malloc(num_blocks_w * sizeof(short*));
+                qF_Cr[m] = malloc(num_blocks_w * sizeof(short*));
+                for (int n=0; n<num_blocks_w; n++) {
+                    qF_Y[m][n]  = calloc(64, sizeof(short));
+                    qF_Cb[m][n] = calloc(64, sizeof(short));
+                    qF_Cr[m][n] = calloc(64, sizeof(short));
+                }
+            }
+
+            // Read RLE binary data
+            for (int by=0; by<num_blocks_h; by++) {
+                for (int bx=0; bx<num_blocks_w; bx++) {
+                    // Y channel
+                    int pos = 0;
+                    while (1) {
+                        short skip, value;
+                        fread(&skip, sizeof(short), 1, fin);
+                        fread(&value, sizeof(short), 1, fin);
+                        if (skip == 0 && value == 0) break;
+                        pos += skip;
+                        if (pos < 64) {
+                            qF_Y[by][bx][pos] = value;
+                            pos++;
+                        }
+                    }
+
+                    // Cb channel
+                    pos = 0;
+                    while (1) {
+                        short skip, value;
+                        fread(&skip, sizeof(short), 1, fin);
+                        fread(&value, sizeof(short), 1, fin);
+                        if (skip == 0 && value == 0) break;
+                        pos += skip;
+                        if (pos < 64) {
+                            qF_Cb[by][bx][pos] = value;
+                            pos++;
+                        }
+                    }
+
+                    // Cr channel
+                    pos = 0;
+                    while (1) {
+                        short skip, value;
+                        fread(&skip, sizeof(short), 1, fin);
+                        fread(&value, sizeof(short), 1, fin);
+                        if (skip == 0 && value == 0) break;
+                        pos += skip;
+                        if (pos < 64) {
+                            qF_Cr[by][bx][pos] = value;
+                            pos++;
+                        }
+                    }
+                }
+            }
+            fclose(fin);
+
+            // Un-DPCM
+            for (int by=1; by<num_blocks_h; by++) {
+                qF_Y[by][0][0]  += qF_Y[by-1][0][0];
+                qF_Cb[by][0][0] += qF_Cb[by-1][0][0];
+                qF_Cr[by][0][0] += qF_Cr[by-1][0][0];
+            }
+            for (int by=0; by<num_blocks_h; by++) {
+                for (int bx=1; bx<num_blocks_w; bx++) {
+                    qF_Y[by][bx][0]  += qF_Y[by][bx-1][0];
+                    qF_Cb[by][bx][0] += qF_Cb[by][bx-1][0];
+                    qF_Cr[by][bx][0] += qF_Cr[by][bx-1][0];
+                }
+            }
+
+            unsigned char QtY[8][8] = {
+                {16,11,10,16,24,40,51,61},
+                {12,12,14,19,26,58,60,55},
+                {14,13,16,24,40,57,69,56},
+                {14,17,22,29,51,87,80,62},
+                {18,22,37,56,68,109,103,77},
+                {24,35,55,64,81,104,113,92},
+                {49,64,78,87,103,121,120,101},
+                {72,92,95,98,112,100,103,99}
+            };
+
+            unsigned char QtC[8][8] = {
+                {17,18,24,47,99,99,99,99},
+                {18,21,26,66,99,99,99,99},
+                {24,26,56,99,99,99,99,99},
+                {47,66,99,99,99,99,99,99},
+                {99,99,99,99,99,99,99,99},
+                {99,99,99,99,99,99,99,99},
+                {99,99,99,99,99,99,99,99},
+                {99,99,99,99,99,99,99,99}
+            };
+
+            unsigned char **image = malloc(height * sizeof(unsigned char*));
+            for (int i=0; i<height; i++) image[i] = malloc(width * 3);
+
+                    // Decode each block
+            for (int by=0; by<num_blocks_h; by++) {
+                for (int bx=0; bx<num_blocks_w; bx++) {
+                    double FY[8][8], FCb[8][8], FCr[8][8];
+                    double bY[8][8], bCb[8][8], bCr[8][8];
+
+                    // 先把 zigzag 順序的資料轉回 row-major 順序
+                    short tempY[64], tempCb[64], tempCr[64];
+                    
+                    // Un-zigzag: qF_Y[by][bx][k] 是 zigzag 順序的第 k 個值
+                    // 應該放回 zigzag[k] 這個位置（row-major index）
+                    for (int k=0; k<64; k++) {
+                        int pos = zigzag[k];  // 這是在 64 個元素中的位置
+                        tempY[pos]  = qF_Y[by][bx][k];
+                        tempCb[pos] = qF_Cb[by][bx][k];
+                        tempCr[pos] = qF_Cr[by][bx][k];
+                    }
+
+                    // 現在把 row-major 的資料轉成 2D 並反量化
+                    for (int i=0; i<8; i++) {
+                        for (int j=0; j<8; j++) {
+                            FY[i][j]  = (double)tempY[i*8+j] * QtY[i][j];
+                            FCb[i][j] = (double)tempCb[i*8+j] * QtC[i][j];
+                            FCr[i][j] = (double)tempCr[i*8+j] * QtC[i][j];
+                        }
+                    }
+
+                    IDCT8x8(FY, bY);
+                    IDCT8x8(FCb, bCb);
+                    IDCT8x8(FCr, bCr);
+
+                    for (int i=0; i<8; i++) {
+                        for (int j=0; j<8; j++) {
+                            int py = by*8+i, px = bx*8+j;
+                            if (py<height && px<width) {
+                                unsigned char R, G, B;
+                                YCbCr2RGB(bY[i][j], bCb[i][j], bCr[i][j], &R, &G, &B);
+                                image[py][px*3+0] = B;
+                                image[py][px*3+1] = G;
+                                image[py][px*3+2] = R;
+                            }
+                        }
+                    }
+                }
+            }
+
+            write_bmp(argv[2], image, width, height);
+
+            for (int i=0; i<height; i++) free(image[i]);
+            free(image);
+
+            for (int m=0; m<num_blocks_h; m++) {
+                for (int n=0; n<num_blocks_w; n++) {
+                    free(qF_Y[m][n]);
+                    free(qF_Cb[m][n]);
+                    free(qF_Cr[m][n]);
+                }
+                free(qF_Y[m]);
+                free(qF_Cb[m]);
+                free(qF_Cr[m]);
+            }
+            free(qF_Y);
+            free(qF_Cb);
+            free(qF_Cr);
+        }
     }
 
     return 0;
--- encoder.c
+++ encoder.c
@@ -204,6 +204,266 @@ int main(int argc, char *argv[])
         fclose(fqY); fclose(fqCb); fclose(fqCr);
         fclose(feY); fclose(feCb); fclose(feCr);
     }
+    /* ===================== Method 2 ===================== */
+    else if (method == 2) {
+        if (argc != 5) return 1;
+
+        FILE *fp = fopen(argv[2], "rb");
+        if (!fp) return 1;
+
+        unsigned char header[54];
+        fread(header, 1, 54, fp);
+        int width  = *(int*)&header[18];
+        int height = *(int*)&header[22];
+        int rowSize = (width * 3 + 3) & ~3;
+
+        unsigned char **image = malloc(height * sizeof(unsigned char*));
+        for (int i=0;i<height;i++)
+            image[i] = malloc(width * 3);
+
+        for (int y=0;y<height;y++){
+            fseek(fp, 54 + (height-1-y)*rowSize, SEEK_SET);
+            fread(image[y],1,width*3,fp);
+        }
+        fclose(fp);
+
+        int num_blocks_h = (height + 7)/8;
+        int num_blocks_w = (width  + 7)/8;
+
+        // Zigzag table
+        int zigzag[64] = {
+            0,  1,  5,  6, 14, 15, 27, 28,
+            2,  4,  7, 13, 16, 26, 29, 42,
+            3,  8, 12, 17, 25, 30, 41, 43,
+            9, 11, 18, 24, 31, 40, 44, 53,
+           10, 19, 23, 32, 39, 45, 52, 54,
+           20, 22, 33, 38, 46, 51, 55, 60,
+           21, 34, 37, 47, 50, 56, 59, 61,
+           35, 36, 48, 49, 57, 58, 62, 63
+        };
+
+        // 先做 DCT 和量化，存到記憶體
+        short ***qF_Y  = malloc(num_blocks_h * sizeof(short**));
+        short ***qF_Cb = malloc(num_blocks_h * sizeof(short**));
+        short ***qF_Cr = malloc(num_blocks_h * sizeof(short**));
+
+        for (int m=0; m<num_blocks_h; m++) {
+            qF_Y[m]  = malloc(num_blocks_w * sizeof(short*));
+            qF_Cb[m] = malloc(num_blocks_w * sizeof(short*));
+            qF_Cr[m] = malloc(num_blocks_w * sizeof(short*));
+            for (int n=0; n<num_blocks_w; n++) {
+                qF_Y[m][n]  = malloc(64 * sizeof(short));
+                qF_Cb[m][n] = malloc(64 * sizeof(short));
+                qF_Cr[m][n] = malloc(64 * sizeof(short));
+            }
+        }
+
+        // 對每個 8x8 區塊進行 DCT 和量化
+        for (int by=0; by<num_blocks_h; by++) {
+            for (int bx=0; bx<num_blocks_w; bx++) {
+                double blockY[8][8] = {0};
+                double blockCb[8][8] = {0};
+                double blockCr[8][8] = {0};
+                double FY[8][8], FCb[8][8], FCr[8][8];
+
+                for (int i=0; i<8; i++) {
+                    for (int j=0; j<8; j++) {
+                        int py = by*8+i;
+                        int px = bx*8+j;
+                        if (py>=height) py=height-1;
+                        if (px>=width)  px=width-1;
+
+                        unsigned char B=image[py][px*3+0];
+                        unsigned char G=image[py][px*3+1];
+                        unsigned char R=image[py][px*3+2];
+
+                        RGB2YCbCr(R,G,B,&blockY[i][j],&blockCb[i][j],&blockCr[i][j]);
+                    }
+                }
+
+                DCT8x8(blockY,FY);
+                DCT8x8(blockCb,FCb);
+                DCT8x8(blockCr,FCr);
+
+                // 量化並存成一維陣列（還沒 zigzag）
+                short tempY[64], tempCb[64], tempCr[64];
+                for (int i=0; i<8; i++) {
+                    for (int j=0; j<8; j++) {
+                        tempY[i*8+j]  = (short)round(FY[i][j]/QtY[i][j]);
+                        tempCb[i*8+j] = (short)round(FCb[i][j]/QtC[i][j]);
+                        tempCr[i*8+j] = (short)round(FCr[i][j]/QtC[i][j]);
+                    }
+                }
+
+                // Zigzag 掃描
+                for (int k=0; k<64; k++) {
+                    qF_Y[by][bx][k]  = tempY[zigzag[k]];
+                    qF_Cb[by][bx][k] = tempCb[zigzag[k]];
+                    qF_Cr[by][bx][k] = tempCr[zigzag[k]];
+                }
+            }
+        }
+
+        for(int i=0;i<height;i++) free(image[i]);
+        free(image);
+
+        // DPCM for DC coefficients
+        for (int by=0; by<num_blocks_h; by++) {
+            for (int bx=num_blocks_w-1; bx>=1; bx--) {
+                qF_Y[by][bx][0]  -= qF_Y[by][bx-1][0];
+                qF_Cb[by][bx][0] -= qF_Cb[by][bx-1][0];
+                qF_Cr[by][bx][0] -= qF_Cr[by][bx-1][0];
+            }
+        }
+        for (int by=num_blocks_h-1; by>=1; by--) {
+            qF_Y[by][0][0]  -= qF_Y[by-1][0][0];
+            qF_Cb[by][0][0] -= qF_Cb[by-1][0][0];
+            qF_Cr[by][0][0] -= qF_Cr[by-1][0][0];
+        }
+
+        // 判斷是 ascii 還是 binary
+        int is_ascii = (strcmp(argv[3], "ascii") == 0);
+
+if (is_ascii) {
+            // Method 2(a): ASCII format
+            FILE *fout = fopen(argv[4], "w");
+            fprintf(fout, "%d %d\n", width, height);
+
+            for (int by=0; by<num_blocks_h; by++) {
+                for (int bx=0; bx<num_blocks_w; bx++) {
+                    // Y channel
+                    fprintf(fout, "(%d,%d, Y)", by, bx);
+                    int skip = 0;
+                    for (int k=0; k<64; k++) {
+                        if (qF_Y[by][bx][k] == 0) {
+                            skip++;
+                        } else {
+                            fprintf(fout, " %d %d", skip, qF_Y[by][bx][k]);
+                            skip = 0;
+                        }
+                    }
+                    fprintf(fout, " 0 0\n");
+
+                    // Cb channel
+                    fprintf(fout, "(%d,%d, Cb)", by, bx);
+                    skip = 0;
+                    for (int k=0; k<64; k++) {
+                        if (qF_Cb[by][bx][k] == 0) {
+                            skip++;
+                        } else {
+                            fprintf(fout, " %d %d", skip, qF_Cb[by][bx][k]);
+                            skip = 0;
+                        }
+                    }
+                    fprintf(fout, " 0 0\n");
+
+                    // Cr channel
+                    fprintf(fout, "(%d,%d, Cr)", by, bx);
+                    skip = 0;
+                    for (int k=0; k<64; k++) {
+                        if (qF_Cr[by][bx][k] == 0) {
+                            skip++;
+                        } else {
+                            fprintf(fout, " %d %d", skip, qF_Cr[by][bx][k]);
+                            skip = 0;
+                        }
+                    }
+                    fprintf(fout, " 0 0\n");
+                }
+            }
+            fclose(fout);
+        } else {
+            // Method 2(b): Binary format
+            FILE *fout = fopen(argv[4], "wb");
+
+            // 寫入寬高
+            fwrite(&width, sizeof(int), 1, fout);
+            fwrite(&height, sizeof(int), 1, fout);
+
+            // 對每個 block 做 RLE
+            for (int by=0; by<num_blocks_h; by++) {
+                for (int bx=0; bx<num_blocks_w; bx++) {
+                    // Y channel
+                    int k = 0;
+                    while (k < 64) {
+                        short skip = 0;
+                        while (k < 64 && qF_Y[by][bx][k] == 0) {
+                            skip++;
+                            k++;
+                        }
+                        if (k < 64) {
+                            fwrite(&skip, sizeof(short), 1, fout);
+                            fwrite(&qF_Y[by][bx][k], sizeof(short), 1, fout);
+                            k++;
+                        } else {
+                            short eob[2] = {0, 0};
+                            fwrite(eob, sizeof(short), 2, fout);
+                            break;  // 下面就不會再寫 EOB
+                        }
+                    }
+                    // Cb channel  
+                    k = 0;
+                    while (k < 64) {
+                        short skip = 0;
+                        while (k < 64 && qF_Cb[by][bx][k] == 0) {
+                            skip++;
+                            k++;
+                        }
+                        if (k < 64) {
+                            fwrite(&skip, sizeof(short), 1, fout);
+                            fwrite(&qF_Cb[by][bx][k], sizeof(short), 1, fout);
+                            k++;
+                        } else {
+                            short eob[2] = {0, 0};
+                            fwrite(eob, sizeof(short), 2, fout);
+                            break;
+                        }
+                    }
+                    // 刪除！
+
+                    // Cr channel
+                    k = 0;
+                    while (k < 64) {
+                        short skip = 0;
+                        while (k < 64 && qF_Cr[by][bx][k] == 0) {
+                            skip++;
+                            k++;
+                        }
+                        if (k < 64) {
+                            fwrite(&skip, sizeof(short), 1, fout);
+                            fwrite(&qF_Cr[by][bx][k], sizeof(short), 1, fout);
+                            k++;
+                        } else {
+                            short eob[2] = {0, 0};
+                            fwrite(eob, sizeof(short), 2, fout);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            long file_size = ftell(fout);
+            fclose(fout);
+            
+            long original_size = width * height * 3;
+            double compression_ratio = (double)original_size / file_size;
+        }
+        
+        // 釋放記憶體
+        for (int m=0; m<num_blocks_h; m++) {
+            for (int n=0; n<num_blocks_w; n++) {
+                free(qF_Y[m][n]);
+                free(qF_Cb[m][n]);
+                free(qF_Cr[m][n]);
+            }
+            free(qF_Y[m]);
+            free(qF_Cb[m]);
+            free(qF_Cr[m]);
+        }
+        free(qF_Y);
+        free(qF_Cb);
+        free(qF_Cr);
+    }
 
     return 0;
 }
